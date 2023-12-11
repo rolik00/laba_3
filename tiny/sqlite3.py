dab618d54a49ab10e55c58d4be3d29ad68c34c85
rolik00 <148611487+rolik00@users.noreply.github.com>
2023-12-11 20:12:41 +0300

    Update sqlite3.py

--- tiny/sqlite3.py
+++ tiny/sqlite3.py
@@ -13,49 +13,54 @@ q1 = 'SELECT VendorID, count(*) FROM trips GROUP BY 1;'
 q2 = 'SELECT passenger_count, avg(total_amount) FROM trips GROUP BY 1;'
 q3 = '''SELECT passenger_count, strftime('%Y', tpep_pickup_datetime), count(*) FROM trips GROUP BY 1, 2;'''
 q4 = '''SELECT passenger_count, strftime('%Y', tpep_pickup_datetime), round(trip_distance), count(*) FROM trips GROUP BY 1, 2, 3 ORDER BY 2, 4 desc;'''
+
 sum = 0
+print("First query")
 for i in range(10):
     t0 = time.perf_counter()
     cursor.execute(q1)
     result = cursor.fetchall()
     t1 = time.perf_counter()
     sum += (t1 - t0)
-result = cursor.execute(q1).fetchall()
-print("First query")
-print("Result: ", result)
-print("Average time: ", sum / 10)
+    if i == 9:
+        print("Result: ", result)
+        print("Average time: ", sum / 10)
+
 sum = 0
+print("Second query")
 for i in range(10):
     t0 = time.perf_counter()
     cursor.execute(q2)
     result = cursor.fetchall()
     t1 = time.perf_counter()
     sum += (t1 - t0)
-result = cursor.execute(q2).fetchall()
-print("Second query")
-print("Result: ", result)
-print("Average time: ", sum / 10)
+    if i == 9:
+        print("Result: ", result)
+        print("Average time: ", sum / 10)
+
 sum = 0
+print("Third query")
 for i in range(10):
     t0 = time.perf_counter()
     cursor.execute(q3)
     result = cursor.fetchall()
     t1 = time.perf_counter()
     sum += (t1 - t0)
-result = cursor.execute(q3).fetchall()
-print("Third query")
-print("Result: ", result)
-print("Average time: ", sum / 10)
+    if i == 9:
+        print("Result: ", result)
+        print("Average time: ", sum / 10)
+
 sum = 0
+print("Forth query")
 for i in range(10):
     t0 = time.perf_counter()
     cursor.execute(q4)
     result = cursor.fetchall()
     t1 = time.perf_counter()
     sum += (t1 - t0)
-result = cursor.execute(q4).fetchall()
-print("Forth query")
-print("Result: ", result)
-print("Average time: ", sum / 10)
+    if i == 9:
+        print("Result: ", result)
+        print("Average time: ", sum / 10)
+
 cursor.close()
 conn.close()
