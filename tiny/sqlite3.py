1dccc443fb41605fc3758ad5d2fe3c47a14307e8
rolik00 <148611487+rolik00@users.noreply.github.com>
2023-12-11 20:00:39 +0300

    Create sqlite3.py

---
+++ tiny/sqlite3.py
@@ -0,0 +1,60 @@
+import time
+import sqlite3
+import pandas as pd
+
+#Creation database
+#data = pd.read_csv(r"D:\Study\бд\laba_3\nyc_yellow_tiny.csv")
+#data.to_sql('trips', conn, if_exists='replace', index=False)
+
+conn = sqlite3.connect('nyc_yellow_tiny.db')
+cursor = conn.cursor()
+q1 = 'SELECT VendorID, count(*) FROM trips GROUP BY 1;'
+q2 = 'SELECT passenger_count, avg(total_amount) FROM trips GROUP BY 1;'
+q3 = '''SELECT passenger_count, strftime('%Y', tpep_pickup_datetime), count(*) FROM trips GROUP BY 1, 2;'''
+q4 = '''SELECT passenger_count, strftime('%Y', tpep_pickup_datetime), round(trip_distance), count(*) FROM trips GROUP BY 1, 2, 3 ORDER BY 2, 4 desc;'''
+sum = 0
+for i in range(10):
+    t0 = time.perf_counter()
+    cursor.execute(q1)
+    result = cursor.fetchall()
+    t1 = time.perf_counter()
+    sum += (t1 - t0)
+result = cursor.execute(q1).fetchall()
+print("First query")
+print("Result: ", result)
+print("Average time: ", sum / 10)
+sum = 0
+for i in range(10):
+    t0 = time.perf_counter()
+    cursor.execute(q2)
+    result = cursor.fetchall()
+    t1 = time.perf_counter()
+    sum += (t1 - t0)
+result = cursor.execute(q2).fetchall()
+print("Second query")
+print("Result: ", result)
+print("Average time: ", sum / 10)
+sum = 0
+for i in range(10):
+    t0 = time.perf_counter()
+    cursor.execute(q3)
+    result = cursor.fetchall()
+    t1 = time.perf_counter()
+    sum += (t1 - t0)
+result = cursor.execute(q3).fetchall()
+print("Third query")
+print("Result: ", result)
+print("Average time: ", sum / 10)
+sum = 0
+for i in range(10):
+    t0 = time.perf_counter()
+    cursor.execute(q4)
+    result = cursor.fetchall()
+    t1 = time.perf_counter()
+    sum += (t1 - t0)
+result = cursor.execute(q4).fetchall()
+print("Forth query")
+print("Result: ", result)
+print("Average time: ", sum / 10)
+cursor.close()
+conn.close()
